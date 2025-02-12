961aedcce732815f7d73b81cf2bf5394fc2dc539
lining <1911891560@qq.com>
2025-02-12 17:55:55 +0800

    add cupertino

--- lib/src/demo/demo_page.dart
+++ lib/src/demo/demo_page.dart
@@ -1,24 +1,407 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-class DemoPage extends StatefulWidget {
-  const DemoPage({super.key});
+import '../sample_feature/sample_item_details_view.dart';
+import '../sample_feature/sample_item_list_view.dart';
+import '../settings/settings_controller.dart';
+import '../settings/settings_view.dart';
 
-  @override
-  State<DemoPage> createState() => _DemoPageState();
+import 'package:http/http.dart' as http;
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({required this.userId, required this.id, required this.title});
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+      'userId': int userId,
+      'id': int id,
+      'title': String title,
+      } =>
+          Album(
+            userId: userId,
+            id: id,
+            title: title,
+          ),
+      _ => throw const FormatException('Failed to load album.'),
+    };
+  }
 }
 
-class _DemoPageState extends State<DemoPage> {
+/// The Widget that configures your application.
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+    required this.settingsController,
+  });
+
+  final SettingsController settingsController;
+
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     backgroundColor: Colors.blue,
-     appBar: AppBar(
-       title: Text('DemoPage')
-     ),
-    body: ListView.builder(itemBuilder: (context, index) {
+    // Glue the SettingsController to the MaterialApp.
+    //
+    // The ListenableBuilder Widget listens to the SettingsController for changes.
+    // Whenever the user updates their settings, the MaterialApp is rebuilt.
+    return ListenableBuilder(
+      listenable: settingsController,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          // Providing a restorationScopeId allows the Navigator built by the
+          // MaterialApp to restore the navigation stack when a user leaves and
+          // returns to the app after it has been killed while running in the
+          // background.
+            restorationScopeId: 'app',
 
-    }),
-   );
+            // Provide the generated AppLocalizations to the MaterialApp. This
+            // allows descendant Widgets to display the correct translations
+            // depending on the user's locale.
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale('en', ''), // English, no country code
+            ],
+
+            // Use AppLocalizations to configure the correct application title
+            // depending on the user's locale.
+            //
+            // The appTitle is defined in .arb files found in the localization
+            // directory.
+            onGenerateTitle: (BuildContext context) =>
+            AppLocalizations.of(context)!.appTitle,
+
+            // Define a light and dark color theme. Then, read the user's
+            // preferred ThemeMode (light, dark, or system default) from the
+            // SettingsController to display the correct theme.
+            theme: ThemeData(
+                useMaterial3: true,
+                textTheme: TextTheme(
+                  displayLarge: const TextStyle(
+                    fontSize: 72,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                colorScheme: ColorScheme.fromSeed(
+                  seedColor: Colors.tealAccent,
+                  brightness: Brightness.light,
+                )),
+            darkTheme: ThemeData.dark(),
+            themeMode: settingsController.themeMode,
+
+            // Define a function to handle named routes in order to support
+            // Flutter web url navigation and deep linking.
+            onGenerateRoute: (RouteSettings routeSettings) {
+              return MaterialPageRoute<void>(
+                settings: routeSettings,
+                builder: (BuildContext context) {
+                  switch (routeSettings.name) {
+                    case SettingsView.routeName:
+                      return SettingsView(controller: settingsController);
+                    case SampleItemDetailsView.routeName:
+                      return const SampleItemDetailsView();
+                    case SampleItemListView.routeName:
+                    default:
+                      return const SampleItemListView();
+                  }
+                },
+              );
+            },
+
+            home: HomePage()
+        );
+      },
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(tabs: [
+            Tab(icon: Icon(Icons.directions_car)),
+            Tab(icon: Icon(Icons.directions_transit)),
+            Tab(icon: Icon(Icons.directions_bike)),
+          ]),
+          title: Text('Tabs demo'),
+          leading: Builder(builder: (context) {
+            return IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          }),
+        ),
+        body: TabBarView(children: [
+          FirstRoute(),
+          TodosScreen(todos: todos),
+          SelectionButton()
+        ]),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              Builder(builder: (context) {
+                return DrawerHeader(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary),
+                  child: Text(
+                    'Drawer Header',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                );
+              }),
+              ListTile(
+                title: Text('主题切换'),
+                onTap: () {
+                  showDialog(context: context, builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('AlertDialog title'),
+                      content: SingleChildScrollView(
+                        child: ListBody(
+                          children: [
+                            Text('This is a demo dialog!'),
+                            Text('Would you like to approve of this message?')
+                          ],
+                        ),
+                      ),
+                      actions: [
+                        TextButton(onPressed: () {
+                          Navigator.of(context).pop();
+                        }, child: Text('Approve'))
+                      ],
+                    );
+                  });
+                },
+              ),
+              ListTile(
+                title: Text('Item2'),
+                onTap: () {},
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('First route'),
+      ),
+      child: Center(
+        child: CupertinoButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                      builder: (context) => const SecondRoute()));
+            },
+            child: const Text('Open route')),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go back!')),
+      ),
+    );
+  }
+}
+
+class Todo {
+  final String title;
+  final String description;
+
+  const Todo(this.title, this.description);
+}
+
+final todos = List.generate(
+    20,
+        (i) =>
+        Todo('Todo $i', 'A description of what needs to be done for Todo $i'));
+
+class TodosScreen extends StatelessWidget {
+  const TodosScreen({super.key, required this.todos});
+
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Todos')),
+      body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(todos[index].title),
+              onTap: () {
+                // Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //         builder: (context) =>
+                //             DetailScreen(todo: todos[index])));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DetailScreen(),
+                        settings: RouteSettings(arguments: todos[index])));
+              },
+            );
+          }),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  // const DetailScreen({super.key, required this.todo});
+  //
+  // final Todo todo;
+  //
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(title: Text(todo.title)),
+  //     body: Padding(padding: EdgeInsets.all(16), child: Text(todo.description)),
+  //   );
+  // }
+
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final todo = ModalRoute.of(context)!.settings.arguments as Todo;
+
+    return Scaffold(
+      appBar: AppBar(title: Text(todo.title)),
+      body: Padding(padding: EdgeInsets.all(16), child: Text(todo.description)),
+    );
+  }
+}
+
+class SelectionButton extends StatefulWidget {
+  const SelectionButton({super.key});
+
+  @override
+  State<SelectionButton> createState() => _SelectionButtonState();
+}
+
+class _SelectionButtonState extends State<SelectionButton> {
+  late Future<Album> futureAlbum;
+
+  Future<Album> fetchAlbum() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+    if (response.statusCode == 200) {
+      return Album.fromJson(jsonEncode(response.body) as Map<String, dynamic>);
+    } else {
+      throw Exception('Failed to load Album');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+    print(futureAlbum);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          _navigateAndDisplaySelection(context);
+        },
+        child: const Text('Pick an option, any option！'));
+  }
+
+  Future<void> _navigateAndDisplaySelection(BuildContext context) async {
+    final result = await Navigator.push(context,
+        MaterialPageRoute(builder: (context) => const SelectionScreen()));
+
+    if (!context.mounted) return;
+
+    ScaffoldMessenger.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(SnackBar(content: Text('$result')));
+  }
+}
+
+class SelectionScreen extends StatelessWidget {
+  const SelectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pick an option'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context, 'Yep!');
+                  },
+                  child: const Text('Yep!')),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context, 'Nope.');
+                  },
+                  child: const Text('Nope.')),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
